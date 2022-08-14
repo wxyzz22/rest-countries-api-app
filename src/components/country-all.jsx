f2249980ac2f89ac2ce8c9c84c867cd89b38658f
wxyzz22 <hsingjawang@gmail.com>
2022-08-14 09:40:26 -0500

    fix filtering (forgot to add the functionality last time)

--- src/components/country-all.jsx
+++ src/components/country-all.jsx
@@ -6,39 +6,55 @@ import SearchRow from './search-row';
 export default function CountryAll() {
     const [res, setRes] = React.useState([]);
     const [search, setSearch] = React.useState('');
+    const [filter, setFilter] = React.useState('');
 
     // function for testing query fetching results
 
-    // const handleClickWindow = (e) => {
-    //     fetch(`https://restcountries.com/v3.1/alpha/AQ`)
-    //     .then((result) => result.json())
-    //     .then((result) => {
-    //         console.log(result);
-    //     })
-    //     .catch(() => "Error occured!");
+    // const handleClickWindow = () => {
+    //     // fetch(`https://restcountries.com/v3.1/alpha/AQ`)
+    //     // .then((result) => result.json())
+    //     // .then((result) => {
+    //     //     console.log(result);
+    //     // })
+    //     // .catch(() => "Error occured!");
+    //     console.log(res.filter((item) => item.region === 'Asia'));
     // };
 
     React.useEffect(() => {
         fetch(`https://restcountries.com/v3.1/${search === '' ? 'all' : `name/${search}`}?fields=altSpellings,flags,name,population,region,capital`)
         .then((result) => result.json())
         .then((result) => {
+            console.log(filter);
+            if (filter) {
+                const newRes = result.filter((item) => item.region === filter);
+                setRes(newRes);
+            } else {
+                setRes(result);
+            }
             // setRes(result.slice(0, 8));
-            setRes(result);
             // console.log(search);
         })
         .catch(() => "Error occured!");
-    }, [search]);
+    }, [search, filter]);
 
     // console.log(search[0]);
     
     const handleInput = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         setSearch(e.target.value);
     };
 
+    const handleFilter = (e) => {
+        if (e.target.id === filter) {
+            setFilter('');
+        } else {
+            setFilter(e.target.id);
+        }
+    }
+
     return (
         <div className='main-page'>
-            <SearchRow inputHandler={handleInput} />
+            <SearchRow inputHandler={handleInput} filterHandler={handleFilter} filter={filter} search={search} />
             {/* Here displays all windows of countries.
             <button onClick={handleClickWindow}>Click to access API</button> */}
             <div className='display-all'>
--- src/components/search-row.jsx
+++ src/components/search-row.jsx
@@ -2,23 +2,34 @@ import * as React from 'react';
 import { BiSearchAlt2 } from "react-icons/bi";
 import { IoChevronDownSharp } from "react-icons/io5";
 
-export default function SearchRow({inputHandler}) {
+export default function SearchRow({inputHandler, filterHandler, filter, search}) {
     return (
         <div className='search-row'>
           <div className='search-field'>
             <BiSearchAlt2 className='font-18' />
-            <input type='text' placeholder='Search for a country...' onChange={inputHandler} />
+            <input type='text' placeholder='Search for a country...' onChange={inputHandler} value={search} />
           </div>
           <div className='dropdown'>
             <button className='dropbtn font-14'>Filter by Region <IoChevronDownSharp /></button>
             <div className='dropdown-content'>
-                <div>Africa</div>
-                <div>America</div>
-                <div>Asia</div>
-                <div>Europe</div>
-                <div>Oceania</div>
+              <FilterOption filter={filter} filterHandler={filterHandler} value='Africa' />
+              <FilterOption filter={filter} filterHandler={filterHandler} value='Americas' />
+              <FilterOption filter={filter} filterHandler={filterHandler} value='Asia' />
+              <FilterOption filter={filter} filterHandler={filterHandler} value='Europe' />
+              <FilterOption filter={filter} filterHandler={filterHandler} value='Oceania' />
+                {/* <div className={`${filter === 'Africa' ? 'filter-dark': ''}`} id="Africa" onClick={(e) => filterHandler(e)}>Africa</div>
+                <div className={`${filter === 'Africa' ? 'filter-dark': ''}`} id="Americas" onClick={(e) => filterHandler(e)}>Americas</div>
+                <div className={`${filter === 'Africa' ? 'filter-dark': ''}`} id="Asia" onClick={(e) => filterHandler(e)}>Asia</div>
+                <div className={`${filter === 'Africa' ? 'filter-dark': ''}`} id="Europe" onClick={(e) => filterHandler(e)}>Europe</div>
+                <div className={`${filter === 'Africa' ? 'filter-dark': ''}`} id="Oceania" onClick={(e) => filterHandler(e)}>Oceania</div> */}
             </div>
           </div>
         </div>
     );
 }
+
+const FilterOption = ({filter, filterHandler, value}) => {
+  return (
+    <div className={`${filter === value ? 'filter-dark': ''}`} id={value} onClick={(e) => filterHandler(e)}>{value}</div>
+  );
+};
